0cd026c9b18f1d3931c0e45a29cc8b901acec6af
yuuhi <hainm112123@gmail.com>
2022-08-20 21:38:34 +0700

    add products search & fixed users search

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -8,10 +8,18 @@ module.exports = {
     var page = parseInt(req.query.page) || 1;
     var perPage = 8;
     var products = productsRef.value();
+    if (req.query.q) {
+      var q = req.query.q.toLowerCase();
+      products = products.filter(function(product) {
+        return product.name.toLowerCase().indexOf(q) !== -1 || product.shortName.toLowerCase().indexOf(q) !== -1;
+      });
+    }
     res.render('products/index', {
       products: products.slice((page - 1) * perPage, page * perPage),
       pageIndex: page,
       numPage: ((products.length  - 1) / perPage >> 0) + 1,
+      baseUrl: req.baseUrl,
+      searchValue: req.query.q,
     });
   },
   
@@ -45,5 +53,21 @@ module.exports = {
     }).write();
 
     res.render('products/create');
+  },
+
+  search: function(req, res) {
+    var q = req.query.q.toLowerCase();
+    var matchedProducts = productsRef.value().filter(function(product) {
+      return product.name.toLowerCase().indexOf(q) !== -1 || product.shortName.toLowerCase().indexOf(q) !== -1;
+    });
+    var page = parseInt(req.query.page) || 1;
+    var perPage = 8;
+    res.render('products/index', {
+      products: matchedProducts.slice((page - 1) * perPage, page * perPage),
+      pageIndex: page,
+      numPage: ((matchedProducts.length - 1) / perPage >> 0) + 1,
+      searchValue: req.query.q,
+      baseUrl: req.baseUrl,
+    })
   }
 }
--- routes/products.route.js
+++ routes/products.route.js
@@ -5,6 +5,7 @@ const controller = require('../controller/products.controller') ;
 
 router.get('/', controller.index);
 router.get('/create', controller.create);
+// router.get('/search', controller.search);
 
 router.post('/create', controller.postCreate);
 
